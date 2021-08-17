8f4f3163b7f7e70046b257212715fc67c3f4564b
Q_Q <mfaster@mfaster.com>
2021-08-17 16:52:15 +0700

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+const pp = require('puppeteer')
+
+start = async () => {
+  console.log('Init Sequence')
+  const b = await pp.launch({ headless: false })
+  const p = await b.newPage()
+  await p.goto('https://popcat.click/', { waitUntil: 'networkidle0' })
+  console.log('Ready?')
+
+  for (let i = 0; i < 10000; i++) {
+    await p.click('[id="app"]')
+  }
+}
+
+start()
